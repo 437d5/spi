c175cd5c50d3a0c7b6367cdfaf09abbf91844d03
437d5 <uladzislaukaplan@gmail.com>
2025-01-16 19:41:27 +0300

    add data_former module and tb

---
+++ sim/src/data_former.sv
@@ -0,0 +1,54 @@
+// Module data_former is used to generate
+// data for testing SPI transmitter.
+// When next_count is high module add 1 to cnt.
+// When start_send and ready are high it sends cnt as data
+// and rises signal valid to 1.
+module data_former # (
+    parameter P_DATA_WIDTH = 8
+) (
+    next_count, start_send, 
+    clk_100, s_rst, a_rst,
+    ready, valid, data
+);
+
+    input  logic     next_count;
+    input  logic     start_send;
+    input  logic     clk_100;
+    input  logic     s_rst;
+    input  logic     a_rst;
+    input  logic     ready;
+
+    output logic     valid;
+    output logic [P_DATA_WIDTH - 1:0] data;
+
+    logic [P_DATA_WIDTH - 1:0] cnt;
+
+    always_ff @( posedge clk_100 or posedge a_rst ) begin
+        // Sync reset is prioritized 
+        if (s_rst) begin
+            valid <= '0;
+            cnt   <= '0;
+        // Async reset
+        end else if (a_rst) begin
+            valid <= '0;
+            cnt   <= '0;
+        end else begin
+            
+            if (next_count) begin
+                cnt <= cnt + 1'b1;
+            end
+
+            // TODO: test if valid sets to 0 after sending data
+            if (start_send) begin
+                if (ready) begin
+                    valid <= '1;
+                end else begin
+                    valid <= '0;
+                end
+            end
+        end
+    end
+
+    assign data = cnt;
+
+endmodule
---
+++ sim/src/data_former_tb.sv
@@ -0,0 +1,66 @@
+module data_former_tb;
+
+    localparam int P_DATA_WIDTH = 8;
+
+    logic   next_count, start_send;
+    logic   clk_100;
+    logic   s_rst, a_rst;
+    logic   ready;
+    wire    valid;
+    wire    [P_DATA_WIDTH-1:0] data;
+
+    data_former #(
+        .P_DATA_WIDTH(P_DATA_WIDTH)
+    ) dut (
+        .next_count     (next_count),
+        .start_send     (start_send),
+        .clk_100        (clk_100),
+        .s_rst          (s_rst),
+        .a_rst          (a_rst),
+        .ready          (ready),
+        
+        .valid          (valid),
+        .data           (data)
+    );
+
+    always #5 clk_100 = ~clk_100;
+
+    initial begin
+        next_count     = 0;
+        start_send     = 0;
+        clk_100        = 0;
+        s_rst          = 0;
+        a_rst          = 0;
+        ready          = 0;
+
+        // async reset
+        #10 a_rst      = 1;
+        #10 a_rst      = 0;
+        
+        // start counter
+        #10 next_count = 1;
+        #20 next_count = 0;
+
+        // start sending
+        #10 start_send = 1;
+        #5  ready      = 1;
+        #10 start_send = 0;
+
+        // sync reset
+        #10 s_rst      = 1;
+        #20 s_rst      = 0;
+
+        #10 next_count = 1;
+        #30 next_count = 0;
+        #10 start_send = 1;
+            ready      = 1;
+        #10 start_send = 0;
+
+        // send without ready
+        #10 start_send = 1;
+        #20 start_send = 0;
+
+	#50 $finish;
+    end
+
+endmodule
